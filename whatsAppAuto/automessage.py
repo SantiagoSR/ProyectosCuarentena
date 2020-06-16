40f98a25f0fce578a9e9162aa5676192ca67635b
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-06-16 17:31:08 -0500

    editando proyecto

--- whatsAppAuto/automessage.py
+++ whatsAppAuto/automessage.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client 
  
-account_sid = 'ACbd52b1ffc3bd84b178f6822583f04795' 
-auth_token = '7a117da18d4a9527bbff4ebcfbc4b87c' 
+account_sid = 'ACCOUNT_SID' 
+auth_token = 'AUTH_TOKEN' 
 client = Client(account_sid, auth_token) 
 def send_mms():
 
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
                               body='Hola este es un saludo corto xd',      
-                              to='whatsapp:+573108329407' 
+                              to='whatsapp: +NUMBER' 
                           ) 
  
     print(message.sid)
